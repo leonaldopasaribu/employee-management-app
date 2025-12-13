3cf7e5d3a0d96ccd4398074488e193322295e0c6
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-12-13 11:06:10 +0700

    chore: setup primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +19,8 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    providePrimeNG({
+      theme: { preset: Aura, options: { darkModeSelector: '.app-light' } },
+    }),
   ],
 };
