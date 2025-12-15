1d6b1330220d8a9a609271f0c0d8fcae729e7862
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-12-15 12:12:28 +0700

    feat: add dashboard page

---
+++ src/app/presentations/dashboard/routes/dashboard.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('../containers/dashboard').then(m => m.Dashboard),
+  },
+];
