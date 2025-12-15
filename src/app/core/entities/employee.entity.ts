39cd5de1e39b531df4f50145eb3e9fea3c3601b9
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-12-15 14:23:15 +0700

    feat: add employee entity

---
+++ src/app/core/entities/employee.entity.ts
@@ -0,0 +1,12 @@
+export interface EmployeeEntity {
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthDate: Date;
+  basicSalary: number;
+  status: string;
+  group: string;
+  description: string;
+  id: string;
+}
