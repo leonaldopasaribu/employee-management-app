6381f4d85d9ff0cb182479812dbfddfae17963fe
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-12-15 14:25:48 +0700

    feat: add route url constant

---
+++ src/app/shared/constants/route-url.constant.ts
@@ -0,0 +1,5 @@
+export const DASHBOARD_ROUTE_URL = '/dashboard';
+export const EMPLOYEE_ROUTE_URL = '/employee';
+export const EMPLOYEE_LIST_ROUTE_URL = '/employee/list';
+export const EMPLOYEE_CREATE_ROUTE_URL = '/employee/create';
+export const SIGN_IN_ROUTE_URL = '/sign-in';
