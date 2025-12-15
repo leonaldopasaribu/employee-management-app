7c5249a82d71943772eff9eff4c6a33ed4c7bffc
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-12-15 14:24:03 +0700

    feat: add employee provider

---
+++ src/app/data/employee/employee.provider.ts
@@ -0,0 +1,12 @@
+import { Provider } from '@angular/core';
+import { EmployeeRepository } from '../../core/repositories/employee.repository';
+import { EmployeeMapper } from './employee.mapper';
+import { EmployeeRepositoryImpl } from './employee.repository.impl';
+
+export const EMPLOYEE_REPOSITORY_PROVIDERS: Provider[] = [
+  {
+    provide: EmployeeRepository,
+    useClass: EmployeeRepositoryImpl,
+  },
+  EmployeeMapper,
+];
