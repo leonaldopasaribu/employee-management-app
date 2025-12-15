9e66973fce5a0adf8c400ad8e976d1506aef4c72
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-12-15 14:23:39 +0700

    feat: add employee dto

---
+++ src/app/data/employee/employee.dto.ts
@@ -0,0 +1,12 @@
+export interface EmployeeDto {
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthDate: Date;
+  basicSalary: number;
+  status: string;
+  group: string;
+  description: string;
+  id: string;
+}
